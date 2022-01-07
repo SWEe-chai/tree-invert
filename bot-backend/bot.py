a8b28113a721c8583f43627f05934c0e9a8d6a36
Hai Nguyen <1503.hai.nguyen@gmail.com>
2022-01-07 23:35:16 +0800

    fix 1liner

--- bot-backend/bot.py
+++ bot-backend/bot.py
@@ -70,10 +70,8 @@ def invert(update, context):
         reply_not_tree(update)
 
 def reply_is_tree(update, filename):
-    update.message.reply_text("""\
-    Hey! Looks like your tree is upside down. Here's the tree again in the\
-    correct orientation.
-    """)
+    update.message.reply_text("Hey! Looks like your tree is upside down. Here's the tree again in the "
+    + "correct orientation.")
     update.message.reply_photo(open(filename, 'rb'))
 
 def reply_not_tree(update):
@@ -108,7 +106,6 @@ def main():
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.photo, invert))
-    dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
